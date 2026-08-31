4ccd5237503b1a3dc130be41be785de3a9ba2a42
José Victor Ribeiro Lima <victor2k25lima@gmail.com>
2026-08-31 18:59:28 -0300

    Atualizando funcionalidades

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Certificate;
 use App\Models\Animal;
 use App\Http\Requests\CertificateRequest;
+use App\Models\Consultation;
 use Illuminate\Support\Facades\Storage;
 
 class CertificateController extends Controller
@@ -14,7 +15,7 @@ class CertificateController extends Controller
     {
         $search = $request->get("search");
 
-        $certificates = Certificate::with(['animal.tutor','certificate'])
+        $certificates = Certificate::with(['animal.tutor'])
             ->when($search, function ($query) use ($search) {
                 $query->where('title', 'like', "%{$search}%")
                 ->orWhereHas('animal', function ($q) use ($search){
@@ -26,4 +27,27 @@ class CertificateController extends Controller
             ->withQueryString();
         return view('certificates.index', compact('certificates', 'search'));
     }
+
+    public function create()
+    {
+        $animals = Animal::with('tutor')->orderBy('name', 'asc')->get();
+
+        return view('certificates.create', compact('animals', 'certificates'));
+    }
+
+        public function store(CertificateRequest $request)
+    {
+        $data = $request->validated();
+        
+        if ($request->hasFile('file')) {
+            $data['file_path'] = $request->file('file')->store('certificates', 'public');
+        }
+
+        Certificate::create($data);
+
+        return redirect()
+            ->route('certificates.index')
+            ->with('success', 'Atestado Médico cadastrado e arquivo anexado com sucesso!');
+    }
+    
 }
