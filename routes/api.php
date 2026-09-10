1ae22c31cea4c58473719716437b6b203463d7ae
José Victor Ribeiro Lima <res0146848@aluno.firjansenaisesi.com.br>
2026-09-09 21:30:39 -0300

    feat: Adicionando o DashboardController da API

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\DashboardController;
 
 // Endpoit aberto: Login e Emissão de Tokens
 Route::post('/login', [AuthController::class, 'login']);
@@ -11,4 +12,6 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/dashboard', [DashboardController::class, 'index']);
 });
